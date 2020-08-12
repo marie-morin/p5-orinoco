e97a1e3e5dd298a6faedb329e068afe030ae1442
marie-morin <mariemorin19@gmail.com>
2020-08-12 11:21:16 +0200

    Product page finished

--- frontend/javascript/product.js
+++ frontend/javascript/product.js
@@ -1,2 +1,54 @@
-let adress = Window.location;
-console.log(adress);
+// Find the page's URL et extract ID
+const search = window.location.search;
+const slice = search.split('=')[1];
+
+
+// Div where the product is going
+const destination = document.getElementById("destination");
+// The template used
+const productTemplate = document.getElementById("template");
+
+
+fetch("http://localhost:3000/api/teddies/" + slice)
+    .then((response) => response.json())
+    .then((element) => {
+        console.log(element);
+
+        const {
+            imageUrl,
+            name,
+            description,
+            price,
+            _id,
+            colors
+        } = element;
+
+        if (imageUrl && name && description && price && _id) {
+            const newBear = document.importNode(productTemplate.content, true);
+
+            const bearName = newBear.querySelector(".product__name");
+            bearName.textContent = name;
+
+            const bearDescription = newBear.querySelector(".product__description");
+            bearDescription.textContent = description;
+
+            const bearPrice = newBear.querySelector(".product__price");
+            bearPrice.textContent = `$${price}`;
+
+            const bearImage = newBear.querySelector(".product__image img");
+            bearImage.setAttribute("alt", description);
+            bearImage.src = imageUrl;
+
+            const bearColor = newBear.querySelector(".product__select");
+            colors.forEach(color => {
+                const newColor = document.createElement("option");
+                newColor.textContent = color;
+                newColor.setAttribute("value", color);
+                bearColor.appendChild(newColor);
+            });
+
+            destination.appendChild(newBear);
+
+            document.querySelector(".hero__title").textContent = name;
+        }
+    });
