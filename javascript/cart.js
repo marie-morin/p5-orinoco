21e4cd648f2d9dbb479098a72dccca28238dc1e2
marie-morin <mariemorin19@gmail.com>
2020-09-06 17:34:41 +0200

    Regex bettering

--- javascript/cart.js
+++ javascript/cart.js
@@ -1,64 +1,58 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener("DOMContentLoaded", (event) => {
+  const productsDestination = document.getElementById("destination");
+  const priceDestination = document.querySelector(".total-price");
+  const deleteDestination = document.querySelector(".mainBtn");
+  const form = document.querySelector(".cartForm");
+  const products = [];
+  let totalPrice;
 
-    const productsDestination = document.getElementById("destination");
-    const priceDestination = document.querySelector(".total-price");
-    const deleteDestination = document.querySelector(".mainBtn");
-    const form = document.querySelector(".cartForm");
-    const products = [];
-    let totalPrice;
+  if (!productsDestination || !priceDestination || !deleteDestination || !form)
+    return;
 
-    if (!productsDestination || !priceDestination || !deleteDestination || !form) return;
+  // Searching for data in localStorage
+  if (localStorage.length == 0) {
+    // If localStorage empty, display message
+    productsDestination.innerHTML = "Your cart is empty";
+    productsDestination.classList.add("empty");
+  } else {
+    // If localStorage contains something, create button to be able to empty cart
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete all items";
+    deleteBtn.classList.add("mainBtn__btn");
+    deleteBtn.classList.add("mainBtn__btn--yellow");
+    deleteDestination.appendChild(deleteBtn);
 
-    // Searching for data in localStorage
-    if (localStorage.length == 0) {
-        // If localStorage empty, display message
-        productsDestination.innerHTML = "Your cart is empty";
-        productsDestination.classList.add("empty");
-    } else {
-        // If localStorage contains something, create button to be able to empty cart
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "Delete all items";
-        deleteBtn.classList.add("mainBtn__btn");
-        deleteBtn.classList.add("mainBtn__btn--yellow");
-        deleteDestination.appendChild(deleteBtn);
+    deleteBtn.addEventListener("click", function () {
+      localStorage.clear();
+      location.reload();
+    });
+  }
 
-        deleteBtn.addEventListener("click", function () {
-            localStorage.clear();
-            location.reload();
-        })
-    }
+  // Fetching data
+  const data = getData("http://localhost:3000/api/teddies");
 
-    // Fetching data
-    const data = getData("http://localhost:3000/api/teddies");
+  data
+    .then((data) => {
+      if (!data.length) return;
 
-    data.then(data => {
-            if (!data.length) return;
+      data.forEach((product) => {
+        const { imageUrl, name, description, price, _id } = product;
 
-            data.forEach(product => {
-                const {
-                    imageUrl,
-                    name,
-                    description,
-                    price,
-                    _id
-                } = product;
+        // Looping trougth localStorage to extract "id" key
+        for (let i = 0; i < localStorage.length; i++) {
+          const key = localStorage.key(i);
+          const usableKey = JSON.parse(localStorage.getItem(key));
+          const localID = usableKey.id;
+          const quantity = usableKey.quantity;
+          const localPrice = usableKey.price;
 
-                // Looping trougth localStorage to extract "id" key
-                for (let i = 0; i < localStorage.length; i++) {
-                    const key = localStorage.key(i);
-                    const usableKey = JSON.parse(localStorage.getItem(key));
-                    const localID = usableKey.id;
-                    const quantity = usableKey.quantity;
-                    const localPrice = usableKey.price;
+          // Searching for a match between fetched elements' id and "id" key in localStorage
+          // and display the coresponding element in cart
+          if (localID == _id) {
+            const article = document.createElement("article");
+            article.classList.add("product");
 
-                    // Searching for a match between fetched elements' id and "id" key in localStorage
-                    // and display the coresponding element in cart
-                    if (localID == _id) {
-                        const article = document.createElement("article");
-                        article.classList.add("product");
-
-                        article.innerHTML =
-                            `<div class="product__showoff">
+            article.innerHTML = `<div class="product__showoff">
                             <img class="product__image product__image--fixedWidth" src="${imageUrl}" alt="${description}">
                         </div>
                         <div class="product__infos product__infos--cart">
@@ -74,98 +68,109 @@ window.addEventListener('DOMContentLoaded', (event) => {
                             <a id="${_id}" class="product__remove ${key}" href="../cart/index.html">Remove</a>
                         </div>`;
 
-                        productsDestination.appendChild(article);
+            productsDestination.appendChild(article);
 
-                        // Reporting selected quantity in cart
-                        const options = article.querySelectorAll("#personalisation option");
-                        if (!options) return;
+            // Reporting selected quantity in cart
+            const options = article.querySelectorAll("#personalisation option");
+            if (!options) return;
 
-                        options.forEach(option => {
-                            if (option.value == quantity) {
-                                option.setAttribute("selected", "");
-                            }
-                        })
-
-                        // Reafecting quantity value in localStorage when quantity select button is modify
-                        const select = article.querySelector(".product__select");
-                        if (!select) return;
-
-                        select.addEventListener("change", function (e) {
-                            const orderName = key;
-                            const newQuantity = e.target.value;
-                            if (newQuantity > 0 && newQuantity <= 3) {
-                                const color = usableKey.color;
-                                const orderContent = {
-                                    "name": name,
-                                    "id": localID,
-                                    "quantity": newQuantity,
-                                    "color": color,
-                                    "price": localPrice.toString()
-                                };
-                                localStorage.removeItem(key);
-                                localStorage.setItem(orderName, JSON.stringify(orderContent));
-
-                                // Recalculating total price due
-                                totalPrice = calculPrice(priceDestination);
-                            }
-                        });
-
-                        // Pushing the element's id in products array
-                        products.push(localID);
-
-                        // Removing element from localStorage and "remove" btn is clicked
-                        const removeBtn = article.querySelector(".product__remove");
-                        removeBtn.addEventListener("click", function (e) {
-                            localStorage.removeItem(key);
-                        });
-                    }
-
-                };
+            options.forEach((option) => {
+              if (option.value == quantity) {
+                option.setAttribute("selected", "");
+              }
             });
 
-            // Assigning total price to total price section in DOM
-            totalPrice = calculPrice(priceDestination);
+            // Reafecting quantity value in localStorage when quantity select button is modify
+            const select = article.querySelector(".product__select");
+            if (!select) return;
 
-            // Listening for submit to POST
-            form.addEventListener("submit", function (e) {
-                e.preventDefault();
-                document.querySelector(".product__warning").textContent = "";
-                // Blocking submit process if cart is empty + displaying message
-                if (products.length == 0) {
-                    productsDestination.innerHTML += "<br><br><span class='attention'>Your cart must contain at least one product !</span>";
-                } else {
-                    const firstName = document.getElementById("first").value;
-                    const lastName = document.getElementById("last").value;
-                    const address = document.getElementById("adress").value;
-                    const city = document.getElementById("city").value;
-                    const email = document.getElementById("email").value;
+            select.addEventListener("change", function (e) {
+              const orderName = key;
+              const newQuantity = e.target.value;
+              if (newQuantity > 0 && newQuantity <= 3) {
+                const color = usableKey.color;
+                const orderContent = {
+                  name: name,
+                  id: localID,
+                  quantity: newQuantity,
+                  color: color,
+                  price: localPrice.toString(),
+                };
+                localStorage.removeItem(key);
+                localStorage.setItem(orderName, JSON.stringify(orderContent));
 
-                    const unwantedRegex = /[0-9]+/;
+                // Recalculating total price due
+                totalPrice = calculPrice(priceDestination);
+              }
+            });
 
-                    if (unwantedRegex.test(firstName) || unwantedRegex.test(lastName) || unwantedRegex.test(city)) {
-                        document.querySelector(".product__warning").textContent = "Your first name, last name and city must not contain numbers";
-                    } else {
-                        const contact = {
-                            firstName,
-                            lastName,
-                            address,
-                            city,
-                            email
-                        };
+            // Pushing the element's id in products array
+            products.push(localID);
 
-                        const body = {
-                            contact,
-                            products
-                        };
+            // Removing element from localStorage and "remove" btn is clicked
+            const removeBtn = article.querySelector(".product__remove");
+            removeBtn.addEventListener("click", function (e) {
+              localStorage.removeItem(key);
+            });
+          }
+        }
+      });
 
-                        postData("http://localhost:3000/api/teddies/order", body, totalPrice);
-                    }
-                }
-            })
-        })
-        .catch(err => {
-            console.log(err);
-            productsDestination.innerHTML = "Cart not available";
-            productsDestination.classList.add("empty");
-        })
+      // Assigning total price to total price section in DOM
+      totalPrice = calculPrice(priceDestination);
+
+      // Listening for submit to POST
+      form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        document.querySelector(".product__warning").textContent = "";
+        // Blocking submit process if cart is empty + displaying message
+        if (products.length == 0) {
+          productsDestination.innerHTML +=
+            "<br><br><span class='attention'>Your cart must contain at least one product !</span>";
+        } else {
+          const firstName = document.getElementById("first").value;
+          const lastName = document.getElementById("last").value;
+          const city = document.getElementById("city").value;
+          const address = document.getElementById("adress").value;
+          const email = document.getElementById("email").value;
+
+          const textRegex = /^[_A-z]*((-|\s)*[_A-z])*$/;
+          const adressRegex = /^[_A-z0-9]*((-|\s)*[_A-z0-9])*$/;
+
+          if (
+            textRegex.test(firstName) &&
+            textRegex.test(lastName) &&
+            textRegex.test(city) &&
+            adressRegex.test(address)
+          ) {
+            const contact = {
+              firstName,
+              lastName,
+              address,
+              city,
+              email,
+            };
+
+            const body = {
+              contact,
+              products,
+            };
+
+            postData(
+              "http://localhost:3000/api/teddies/order",
+              body,
+              totalPrice
+            );
+          } else {
+            document.querySelector(".product__warning").textContent =
+              "Your first name, last name and city must not contain numbers";
+          }
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      productsDestination.innerHTML = "Cart not available";
+      productsDestination.classList.add("empty");
+    });
 });
--- javascript/greetings.js
+++ javascript/greetings.js
@@ -1,66 +1,65 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    const mainContainer = document.querySelector(".greetings");
-    if (!mainContainer) return;
+window.addEventListener("DOMContentLoaded", (event) => {
+  const mainContainer = document.querySelector(".greetings");
+  if (!mainContainer) return;
 
-    // Redirecting to home page if page is reload
-    if (localStorage.length == 0) {
-        mainContainer.innerHTML = "";
-        window.location.replace("../../index.html");
-    }
+  // Redirecting to home page if page is reload
+  if (localStorage.length == 0) {
+    mainContainer.innerHTML = "";
+    window.location.replace("../../index.html");
+  }
 
-    // Finding DOM destination elements to fill with data
-    const nameElt = document.querySelector(".greetings__name");
-    const emailElt = document.querySelector(".greetings__email");
-    const priceElt = document.querySelector(".greetings__total");
-    const orderElt = document.querySelectorAll(".greetings__orderId");
-    const destination = document.querySelector(".greetings__destination");
+  // Finding DOM destination elements to fill with data
+  const nameElt = document.querySelector(".greetings__name");
+  const emailElt = document.querySelector(".greetings__email");
+  const priceElt = document.querySelector(".greetings__total");
+  const orderElt = document.querySelectorAll(".greetings__orderId");
+  const destination = document.querySelector(".greetings__destination");
 
-    if (!nameElt || !emailElt || !priceElt || !orderElt || !destination) return;
+  if (!nameElt || !emailElt || !priceElt || !orderElt || !destination) return;
 
-    // Getting informations from localStorage and assigning its values to corresponding DOM elements
-    const orderRecap = JSON.parse(localStorage.getItem('orderRecap'));
+  // Getting informations from localStorage and assigning its values to corresponding DOM elements
+  const orderRecap = JSON.parse(localStorage.getItem("orderRecap"));
 
-    nameElt.textContent = orderRecap.contact.firstName;
-    emailElt.textContent = orderRecap.contact.email;
+  nameElt.textContent = orderRecap.contact.firstName;
+  emailElt.textContent = orderRecap.contact.email;
 
-    orderElt.forEach(element => {
-        element.textContent = orderRecap.orderId;
-    })
+  orderElt.forEach((element) => {
+    element.textContent = orderRecap.orderId;
+  });
 
-    // Getting every product from products recap array
-    const orderedProducts = orderRecap.products;
+  // Getting every product from products recap array
+  const orderedProducts = orderRecap.products;
 
-    // Finding all the possible local keys and placing them in an array
-    let localNames = [];
-    orderedProducts.forEach(product => {
-        product.colors.forEach(color => {
-            let newName = whiteSpaceSupressor(product.name) + whiteSpaceSupressor(color);
-            console.log(newName);
-            if (localNames.indexOf(newName) == -1) localNames.push(newName);
-        })
+  // Finding all the possible local keys and placing them in an array
+  let localNames = [];
+  orderedProducts.forEach((product) => {
+    product.colors.forEach((color) => {
+      let newName =
+        whiteSpaceSupressor(product.name) + whiteSpaceSupressor(color);
+      console.log(newName);
+      if (localNames.indexOf(newName) == -1) localNames.push(newName);
     });
+  });
 
-    // For every element in localStorage, trying to fing a match between the keys and localNames array's entries
-    // If so, insert it in DOM
-    for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        console.log(key);
-        const actualName = JSON.parse(localStorage.getItem(key)).name;
-        console.log(localStorage.getItem(key));
-        const fullColor = JSON.parse(localStorage.getItem(key)).color;
-        const quantity = JSON.parse(localStorage.getItem(key)).quantity;
-        localNames.forEach(name => {
-            if (key == name) {
-                const productElt = document.createElement("div");
-                productElt.classList.add("greetings__product");
-                productElt.innerHTML = `<p>${actualName} : ${fullColor}</p><p>${quantity}</p>`;
-                destination.appendChild(productElt);
-            }
-        })
-    }
+  // For every element in localStorage, trying to fing a match between the keys and localNames array's entries
+  // If so, insert it in DOM
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const actualName = JSON.parse(localStorage.getItem(key)).name;
+    const fullColor = JSON.parse(localStorage.getItem(key)).color;
+    const quantity = JSON.parse(localStorage.getItem(key)).quantity;
+    localNames.forEach((name) => {
+      if (key == name) {
+        const productElt = document.createElement("div");
+        productElt.classList.add("greetings__product");
+        productElt.innerHTML = `<p>${actualName} : ${fullColor}</p><p>${quantity}</p>`;
+        destination.appendChild(productElt);
+      }
+    });
+  }
 
-    priceElt.textContent = localStorage.getItem("price");
+  priceElt.textContent = localStorage.getItem("price");
 
-    // Once content is created, empty localStorage
-    localStorage.clear();
+  // Once content is created, empty localStorage
+  localStorage.clear();
 });
